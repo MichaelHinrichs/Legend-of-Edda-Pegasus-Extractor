d5cffd73f5f3e0e20e6a9efd006dd8ccb78a953f
NintenHero <37460517+MichaelHinrichs@users.noreply.github.com>
2024-03-23 23:49:44 -0300

    Add Super Mecha Champions to eSoftnet games list comment

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿//Written for games in the eSofnet engine.
 //Legend of Edda: Pegasus https://store.steampowered.com/app/2241570/
 //Luna Online: Reborn https://store.steampowered.com/app/457590/
+//Super_Mecha_Champions: https://store.steampowered.com/app/1368910/
+
 using System.IO;
 
 namespace eSofnet_Extractor
